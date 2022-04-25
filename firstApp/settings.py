f8f70b85d65001dc034b5e5f67778767054aea7e
@leoevgeniy <leoevgeniy@gmail.com>
2022-04-25 11:28:10 +0400

    2

--- firstApp/settings.py
+++ firstApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'firstApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'main/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -122,7 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static")
+   os.path.join(BASE_DIR, "main/static")
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
